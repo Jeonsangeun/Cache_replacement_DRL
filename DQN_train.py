9d469afdfc45b1168022203c3219a6f9931811ba
Jeonsangeun <44052428+Jeonsangeun@users.noreply.github.com>
2023-07-19 00:58:07 +0900

    Update DQN_train.py

--- DQN_train.py
+++ DQN_train.py
@@ -66,29 +66,11 @@ def Train(Q, Q_target, memory, optimizer): # training function motive by seungeu
         optimizer.step()
 
 def main():
-    if type_DNN == 0:
-        main_DQN = DNN_model.Qnet_FCN(input_size, node, output_size).to(device)  # FCN
-        target_DQN = DNN_model.Qnet_FCN(input_size, node, output_size).to(device).to(device)
-    elif type_DNN == 1:
-        main_DQN = DNN_model.Qnet_v6(env.Num_packet, env.Num_file, env.F_packet, node, output_size).to(device)  # CNN
-        target_DQN = DNN_model.Qnet_v6(env.Num_packet, env.Num_file, env.F_packet, node, output_size).to(device)
-        
-    if Model_path != "":
-        main_DQN.load_state_dict(torch.load(Model_path, map_location='cpu'))
-        main_DQN.eval()
-
-    target_DQN.load_state_dict(main_DQN.state_dict())
-    target_DQN.eval()
-
-    memory = DNN_model.ReplayBuffer()
-    
     env.Zip_funtion() # init popularity
     request = 1000 # the number of requests
-    interval = 10 # output check cycle
+    interval = 20 # output check cycle
     cost, hit_rate, tr_epoch = 0.0, 0.0, 0 # init cost
-    
-    # optimize tool
-    optimizer = optim.Adam(main_DQN.parameters(), lr=learning_rate)
+    max_episode = 20000
 
     for episode in range(max_episode):
 
@@ -98,12 +80,19 @@ def main():
         for i in range(request * env.Num_packet):
 
             # Proposed scheme
-            s = torch.from_numpy(state).float().unsqueeze(0)
-            with torch.no_grad():
-                aa = DNN_model.Predict_Qnet_v6(main_DQN.eval().cpu(), s).detach().numpy()
-                sigma = max(10.0 / ((episode / 200) + 1), 0.316)  # sigma^2 = 0.1
-                Noise = np.random.normal(0, sigma, size=4 * env.F_packet) / 10
-                action = env.action_select(aa, Noise)
+            aa = 0
+            if type_DNN == 0:
+                s = torch.from_numpy(state).float().unsqueeze(0)
+                with torch.no_grad():
+                    aa = net.Predict_Qnet_FCN(main_DQN.eval().cpu(), s).detach().numpy()
+            elif type_DNN == 1:
+                s = torch.from_numpy(state).float().unsqueeze(0)
+                with torch.no_grad():
+                    aa = net.Predict_Qnet_v6(main_DQN.eval().cpu(), s).detach().numpy()
+
+            sigma = max(10.0 / ((episode / 200) + 1), 0.316)  # sigma^2 = 0.1
+            Noise = np.random.normal(0, sigma, size=4 * env.F_packet) / 10
+            action = env.action_select(aa, Noise)
 
             next_state, reward, done, file, user = env.step(action, file, user)
             done_mask = 0.0 if done else 1.0
@@ -124,11 +113,10 @@ def main():
         hit_rate += env.hit
 
         if episode % interval == (interval - 1): # Training
-            main_DQN.to(device) # GPU learning
+            # main_DQN.to(device) # GPU learning
             latency_layer.append(cost / interval)
             cache_layer.append(hit_rate / interval)
             print("Episode: {} cost: {} hit_rate: {}".format(episode, (cost / interval), (hit_rate / interval)))
-
             Train(main_DQN, target_DQN, memory, optimizer)
             target_DQN.load_state_dict(main_DQN.state_dict())
             target_DQN.eval()
@@ -136,17 +124,17 @@ def main():
 
         if episode % 2500 == 0 and episode != 0: # Saving learned NNs every 2500 (check-point)
             tr_epoch += 1
-            savePath = "test_model_conv" + str(tr_epoch) + ".pth"
+            savePath = "Result/test_model_conv" + str(tr_epoch) + ".pth"
             torch.save(main_DQN.state_dict(), savePath)
-            np.save("acc_delay", latency_layer)
-            np.save("cache_hit", cache_layer)
+            np.save("Result/acc_delay", latency_layer)
+            np.save("Result/cache_hit", cache_layer)
 
     # finished NN
-    savePath = "final_model.pth"
+    savePath = "Result/final_model.pth"
     torch.save(main_DQN.state_dict(), savePath)
 
-    np.save("final_acc_delay", latency_layer)
-    np.save("final_cache_hit", cache_layer)
+    np.save("Result/final_acc_delay", latency_layer)
+    np.save("Result/final_cache_hit", cache_layer)
 
 if __name__ == '__main__':
     main()
